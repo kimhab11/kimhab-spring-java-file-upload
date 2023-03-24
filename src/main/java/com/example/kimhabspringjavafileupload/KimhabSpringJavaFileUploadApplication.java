b9a4efb5b6854ac3cacaf5a1f1cb31dfa66877ed
kimhab <69948342+kimhab11@users.noreply.github.com>
2023-03-24 08:40:59 +0700

    Initial commit

---
+++ src/main/java/com/example/kimhabspringjavafileupload/KimhabSpringJavaFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringjavafileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimhabSpringJavaFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimhabSpringJavaFileUploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kimhabspringjavafileupload/KimhabSpringJavaFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringjavafileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimhabSpringJavaFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
